914601cd3bbc38151c7938d934b72ac1ed6701b7
pagchomp <bmburk@brookdalecc.edu>
2017-05-23 23:13:23 -0400

    Revert "Revert "Shiny app up and running""
    This reverts commit 8d076b5b4ef44a340f8a77ea4127443dc1ecf0a9.

--- ui.R
+++ ui.R
@@ -1,40 +1,21 @@
 library(shiny)
 # Load in the data
-cdc <- read.csv("cleaned-cdc-mortality-1999-2010.csv")
-# Filter so it's only using 2010's data
-cdc <- filter(cdc, Year==2010)
-# Define server logic required to summarize and view the selected dataset
-shinyServer(function(input, output) {
+folder <- "E:\\Projects\\manhattanPrep\\"
+wf <- read.csv(paste0(folder, "cleanedManhattan.csv"), stringsAsFactors = FALSE)
+shinyUI(pageWithSidebar(
   
-  # Return the requested dataset
-  datasetInput <- reactive({
-    # Filter by the chosen cause
-    filter(cdc, ICD.Chapter == input$cause)
-  })
+  headerPanel("Instructor Results"),
+  sidebarPanel(
+    selectInput("code", 
+                "Choose an Instructor Code:",
+                choices = names(table(wf$Instructor)), 
+                selected = "95"),
+    submitButton("Update"),
+    h5("Choose an Instructor to see their results.")
+  ),
   
-  # Generate a summary of the dataset
-  output$barPlot <- renderPlot({
-    to.be.sorted <- datasetInput()
-    # Sorting by crude rate
-    sorted <- to.be.sorted[order(to.be.sorted$Crude.Rate),]
-    sorted$State <- factor(sorted$State, levels=unique(as.character(sorted$State)))
-    # Plotting
-    # Simple error handling
-    if (length(sorted$State) > 0){
-      ggplot(data=sorted, aes(x=State, y=Crude.Rate, fill=Population)) + 
-        geom_bar(stat="identity", position = position_dodge(width=10)) + 
-        coord_flip() + 
-        ylab("Crude Mortality Rate") + 
-        ggtitle(input$cause) +
-        theme_bw()
-    }
-    # Not ideal, but this way nothing has to be changed in the ui file
-    else {
-      # Plots so that it is still returned
-      par(mar = c(0,0,0,0))
-      plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n')
-      text(x = 0.5, y = 0.5, "No data available for 2010", 
-           cex = 1.6, col = "black")
-    }
-  })
-})
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("barPlot", height="800px")
+  )
+))
